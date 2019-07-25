518b2a54842599c941064013836c45b59f761734
Shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-25 12:15:07 +0800

    modify file

--- Billstat.py
+++ Billstat.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# Copyright 2019 Shieber
-# All Rights Reserved.
 #
+#    Author: Shieber
+#
+#                             APACHE LICENSE
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+#
+#                            Function Description
 #    1.按照每年的消费信息建立统计表 
 #    2.按照年月日保存每笔消费信息   
 #    3.保存的信息格式为xlsx         
 #    4.后期将加入按照每月总消费统计信息的功能      
 #    5.后期将加入将统计信息展示为图片的功能         
+#
+#    Copyright 2019 
+#    All Rights Reserved!
 
 import os,re,sys,time
 import requests
